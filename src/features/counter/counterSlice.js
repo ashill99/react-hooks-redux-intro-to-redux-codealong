882ee56ce845e05bb469e4dd11aa096de2f31253
ashill99 <admshng@gmail.com>
2021-02-01 14:26:00 -0500

    Done.

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -5,11 +5,14 @@ const initialState = {
 function counterReducer(state = initialState, action) {
   switch (action.type) {
     case "count/increment":
+      console.log("current state.items length %s", state.items.length)
+      console.log("updating state.items length to %s", state.items.length + 1)
       return {
         ...state,
         items: state.items.concat(state.items.length + 1),
       };
     default:
+      console.log("Initial state.items length %s")
       return state;
   }
 }
